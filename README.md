a9ccb259e6dcf0ac2be053232f35e8978d5303a9
Satvik <satvik.ahuja13@gmail.com>
2023-09-26 23:21:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an Automated Birthday wishing tool
+
+It sends the birthday person an email wishing them 
+
+Data of birthdays to be stored in the csv file
---
+++ main.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "USE OWN"
+MY_PASSWORD = "USE OWN"
+today_tuple = (dt.datetime.now().month,dt.datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+
+
+birthdays_dict = {(data_row["month"],data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject; Happy Birthday! \n\n{contents}")
+
